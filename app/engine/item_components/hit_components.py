777a9a6b122ac4cdfe6d6dc77e726281b68c57b5
Sigmaraven <masterofthecheese@gmail.com>
2023-11-30 23:06:13 -0500

    aaaaa

--- app/engine/item_components/hit_components.py
+++ app/engine/item_components/hit_components.py
@@ -193,7 +193,7 @@ class Shove(ItemComponent):
         mcost = movement_funcs.get_mcost(unit_to_move, new_position)
         if game.board.check_bounds(new_position) and \
                 not game.board.get_unit(new_position) and \
-                mcost <= 90:
+                (mcost <= 90 or skill_system.ignore_terrain(unit_to_move)):
             return new_position
         return False
 
