f5735a15ea779c9315ddb701359b607f25555df5
Sigmaraven <masterofthecheese@gmail.com>
2024-01-15 13:08:55 -0500

    This isn't working out fantastically

--- app/engine/background.py
+++ app/engine/background.py
@@ -49,7 +49,8 @@ class PanoramaBackground():
         self.panorama = panorama
         if not self.panorama.images:
             for path in self.panorama.get_all_paths():
-                image = engine.image_load(path)
+                path_fixed = re.sub(r'^.*?threequel', 'threequel', path)
+                image = engine.image_load(path_fixed)
                 if image.get_size() != (WINWIDTH, WINHEIGHT):
                     image = engine.transform_scale(image, (WINWIDTH, WINHEIGHT))
                 self.panorama.images.append(image)
