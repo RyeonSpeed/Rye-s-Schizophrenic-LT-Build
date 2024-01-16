825f6ccef592110068da71cc33f1166b38002cda
Sigmaraven <masterofthecheese@gmail.com>
2024-01-16 11:40:44 -0500

    Save file fixing(?)

--- app/engine/save.py
+++ app/engine/save.py
@@ -1,4 +1,4 @@
-import os, shutil, glob, re
+import os, shutil, glob, re, shutil
 from datetime import datetime
 from typing import Optional
 import threading
@@ -16,18 +16,26 @@ from app.engine.objects.item import ItemObject
 from app.engine.objects.skill import SkillObject
 
 import logging
+from appdirs import AppDirs
 
 SAVE_THREAD = None
 
 def GAME_NID():
     return str(DB.constants.value('game_nid'))
-
+    
+dirs = AppDirs(GAME_NID(), "Lex Talionis")
+    
 try: 
-    os.mkdir(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'saves'))
+    shutil.move(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'saves'), os.path.join(dirs.user_data_dir, 'saves'))
+except OSError as error: 
+    print(error)
+    
+try: 
+    os.mkdir(os.path.join(dirs.user_data_dir, 'saves'))
 except OSError as error: 
     print(error)
 
-SUSPEND_LOC = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'saves', GAME_NID() + '-suspend.pmeta')
+SUSPEND_LOC = os.path.join(dirs.user_data_dir, 'saves', GAME_NID() + '-suspend.pmeta')
 
 class SaveSlot():
     no_name = '--NO DATA--'
@@ -78,11 +86,11 @@ def dict_print(d):
 
 def save_io(s_dict, meta_dict, old_slot, slot, force_loc=None, name=None):
     if name:
-        save_loc = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'saves', name + '.p')
+        save_loc = os.path.join(dirs.user_data_dir, 'saves', name + '.p')
     elif force_loc:
-        save_loc = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'saves', GAME_NID() + '-' + force_loc + '.p')
+        save_loc = os.path.join(dirs.user_data_dir, 'saves', GAME_NID() + '-' + force_loc + '.p')
     elif slot is not None:
-        save_loc = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'saves', GAME_NID() + '-' + str(slot) + '.p')
+        save_loc = os.path.join(dirs.user_data_dir, 'saves', GAME_NID() + '-' + str(slot) + '.p')
     meta_loc = save_loc + 'meta'
 
     logging.info("Saving to %s", save_loc)
@@ -100,7 +108,7 @@ def save_io(s_dict, meta_dict, old_slot, slot, force_loc=None, name=None):
 
     # For restart
     if not force_loc:
-        r_save = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'saves', GAME_NID() + '-restart' + str(slot) + '.p')
+        r_save = os.path.join(dirs.user_data_dir, 'saves', GAME_NID() + '-restart' + str(slot) + '.p')
         r_save_meta = r_save + 'meta'
         # If the slot I'm overwriting is a start of map
         # Then rename it to restart file
@@ -109,7 +117,7 @@ def save_io(s_dict, meta_dict, old_slot, slot, force_loc=None, name=None):
                 shutil.copy(save_loc, r_save)
                 shutil.copy(meta_loc, r_save_meta)
         elif old_slot is not None:
-            old_name = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'saves', GAME_NID() + '-restart' + str(old_slot) + '.p')
+            old_name = os.path.join(dirs.user_data_dir, 'saves', GAME_NID() + '-restart' + str(old_slot) + '.p')
             old_name_meta = old_name + 'meta'
             if old_name != r_save and os.path.exists(old_name):
                 shutil.copy(old_name, r_save)
@@ -117,11 +125,11 @@ def save_io(s_dict, meta_dict, old_slot, slot, force_loc=None, name=None):
 
     # For preload
     if meta_dict['kind'] == 'start':
-        preload_saves = glob.glob(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'saves', GAME_NID() + '-preload-' + str(meta_dict['level_nid']) + '-*.p'))
+        preload_saves = glob.glob(os.path.join(dirs.user_data_dir, 'saves', GAME_NID() + '-preload-' + str(meta_dict['level_nid']) + '-*.p'))
         nids = [p.split('-')[-1][:-2] for p in preload_saves]
         unique_nid = str(str_utils.get_next_int('0', nids))
-        preload_save = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'saves', GAME_NID() + '-preload-' + str(meta_dict['level_nid']) + '-' + unique_nid + '.p')
-        preload_save_meta = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'saves', GAME_NID() + '-preload-' + str(meta_dict['level_nid']) + '-' + unique_nid + '.pmeta')
+        preload_save = os.path.join(dirs.user_data_dir, 'saves', GAME_NID() + '-preload-' + str(meta_dict['level_nid']) + '-' + unique_nid + '.p')
+        preload_save_meta = os.path.join(dirs.user_data_dir, 'saves', GAME_NID() + '-preload-' + str(meta_dict['level_nid']) + '-' + unique_nid + '.pmeta')
 
         shutil.copy(save_loc, preload_save)
         shutil.copy(meta_loc, preload_save_meta)
@@ -178,7 +186,7 @@ def set_next_uids(game_state):
 def load_saves():
     save_slots = []
     for num in range(0, int(DB.constants.value('num_save_slots'))):
-        meta_fp = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'saves', GAME_NID() + '-' + str(num) + '.pmeta')
+        meta_fp = os.path.join(dirs.user_data_dir, 'saves', GAME_NID() + '-' + str(num) + '.pmeta')
         ss = SaveSlot(meta_fp, num)
         save_slots.append(ss)
     return save_slots
@@ -186,7 +194,7 @@ def load_saves():
 def load_restarts():
     save_slots = []
     for num in range(0, int(DB.constants.value('num_save_slots'))):
-        meta_fp = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'saves', GAME_NID() + '-restart' + str(num) + '.pmeta')
+        meta_fp = os.path.join(dirs.user_data_dir, 'saves', GAME_NID() + '-restart' + str(num) + '.pmeta')
         ss = SaveSlot(meta_fp, num)
         save_slots.append(ss)
     return save_slots
@@ -196,7 +204,7 @@ def get_all_saves():
     Grabs all the turn_change saves
     """
     save_slots = []
-    name = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'saves', GAME_NID() + '-turn_change-*-*.pmeta')
+    name = os.path.join(dirs.user_data_dir, 'saves', GAME_NID() + '-turn_change-*-*.pmeta')
     for meta_fn in glob.glob(name):
         ss = SaveSlot(meta_fn, 0)
         save_slots.append(ss)
@@ -220,9 +228,9 @@ def delete_save(game_state, num: Optional[int] = None):
     if num is None:
         logging.error("delete_save: num not provided and no current save slot")
         return
-    meta_fn = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'saves', GAME_NID() + '-', str(num) + '.pmeta')
-    save_fn = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'saves', GAME_NID() + '-', str(num) + '.p')
-    r_save_fn = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'saves', GAME_NID() + '-restart' + str(num) + '.p')
+    meta_fn = os.path.join(dirs.user_data_dir, 'saves', GAME_NID() + '-', str(num) + '.pmeta')
+    save_fn = os.path.join(dirs.user_data_dir, 'saves', GAME_NID() + '-', str(num) + '.p')
+    r_save_fn = os.path.join(dirs.user_data_dir, 'saves', GAME_NID() + '-restart' + str(num) + '.p')
     if os.path.exists(meta_fn):
         os.remove(meta_fn)
     if os.path.exists(save_fn):
