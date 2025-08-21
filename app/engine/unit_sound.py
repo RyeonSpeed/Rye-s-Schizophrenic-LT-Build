57454230b6bbd0b8e839a0a2189d6bd4e7d9b628
MKCocoon <60154827+MKCocoon@users.noreply.github.com>
2025-08-20 22:51:23 -0600

    misc footsteps
    added mogall, spider, skeleton, zombie footsteps (hardcoding is awesome)

--- app/engine/unit_sound.py
+++ app/engine/unit_sound.py
@@ -5,7 +5,11 @@ class UnitSound():
         'Flier': {0: 'Flier', 20: 'repeat'},
         'Mounted': {0: 'Mounted1', 3: 'Mounted2', 10: 'Mounted3', 21: 'repeat'},
         'Armor': {0: 'Armor1', 16: 'Armor2', 32: 'repeat'},
-        'Infantry': {0: 'Infantry1', 8: 'Infantry2', 16: 'repeat'}}
+        'Infantry': {0: 'Infantry1', 8: 'Infantry2', 16: 'repeat'},
+        'Mogall': {0: 'Mogall', 22: 'repeat'},
+        'Spider': {0: 'Bael', 11: 'repeat'},
+        'Skeleton': {0: 'Skeleton', 10: 'repeat'},
+        'Zombie': {0: 'Zombie', 12: 'repeat'}}
 
     def __init__(self, unit):
         self.unit = unit
@@ -14,7 +18,15 @@ class UnitSound():
         self.playing_sound = 0
 
     def play(self):
-        if 'Flying' in self.unit.tags:
+        if self.unit.klass in ['Spider', 'Spider_Queen', 'Baellista']:
+            self.current_sound = 'Spider'
+        elif self.unit.klass in ['Bigle', 'Bigler', 'Tsukumogami_M', 'Tsukumogami_S']:
+            self.current_sound = 'Mogall'
+        elif self.unit.klass in ['Skeleton', 'Boneguard', 'Wight_King', 'Skeleton_B', 'Boneguard_B']:
+            self.current_sound = 'Skeleton'
+        elif self.unit.klass in ['Revenant', 'Entombed', 'Mermaid']:
+            self.current_sound = 'Zombie'
+        elif 'Flying' in self.unit.tags:
             self.current_sound = 'Flier'
         elif 'Mounted' in self.unit.tags:
             self.current_sound = 'Mounted'
